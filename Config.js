bb1a248a91c6d0b73074165530a80d93355693f9
imaginecreation <imagine.imcd@gmail.com>
2026-08-07 16:29:38 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -4,7 +4,7 @@
  */
 
 const CONFIG = {
-  SPREADSHEET_ID: '1CNTlNGn7w5rRDWundhnNgFaUII9kQvAEBUmWe0lpWGw',
+  SPREADSHEET_ID: '1vzNmd49BiuFT7e-gssRPiPNSoSQ6Q1Biyf9cRiiw57s',
   LIFF_ID: '2009016720-k0zSXOrx', // Replace with actual LIFF ID when deployed
   TIMEZONE: 'Asia/Bangkok',
   RETENTION_DAYS: 10,
